ee252d5e93a38c23c4d28dc889e5d39306632b40
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-09-03 20:32:21 -0400

    Updated to v0.4 Beta

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class SalHackMod implements ModInitializer {
     public static final Logger log = LoggerFactory.getLogger("sal");
 	public static final String NAME = "SalHack";
-	public static final String VERSION = "v0.3 Beta";
+	public static final String VERSION = "v0.4 Beta";
 
 	public static final EventBus EVENT_BUS = new EventManager();
 
