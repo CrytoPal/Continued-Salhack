f877b81a63ce83f8106c7e3c6034c52e473d1417
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-09-05 22:57:10 -0400

    Added MOD_ID

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class SalHackMod implements ClientModInitializer {
     public static final Logger log = LoggerFactory.getLogger("sal");
 	public static final String NAME = "SalHack";
+	public static final String MOD_ID = "sal-hack";
 	public static final String VERSION = "v0.5 Beta";
 
 	public static final EventBus EVENT_BUS = new EventManager();
