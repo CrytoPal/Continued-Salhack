e478ce047f222d044f1c17d76dcf892ad026a362
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-09-08 21:08:58 -0400

    v0.6 Beta Release!

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -1,9 +1,9 @@
 package me.ionar.salhack;
 
 import me.ionar.salhack.main.SalHack;
-import me.zero.alpine.fork.bus.EventManager;
+import me.zero.alpine.bus.EventBus;
+import me.zero.alpine.bus.EventManager;
 import net.fabricmc.api.ClientModInitializer;
-import me.zero.alpine.fork.bus.EventBus;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +12,11 @@ public class SalHackMod implements ClientModInitializer {
     public static final Logger log = LoggerFactory.getLogger("sal");
 	public static final String NAME = "SalHack";
 	public static final String MOD_ID = "sal-hack";
-	public static final String VERSION = "v0.5 Beta";
-	public static final EventBus EVENT_BUS = new EventManager();
+	public static final String VERSION = "v0.6 Beta";
+	public static final EventBus EVENT_BUS = EventManager.builder()
+			.setName("my_application/root") // Descriptive name for the bus
+			.setSuperListeners()            // Enable Listeners to receive subtypes of their target
+			.build();
 
 	@Override
 	public void onInitializeClient() {
